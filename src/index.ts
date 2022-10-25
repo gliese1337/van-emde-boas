27d8766c8431758b6a96a16fb1979bd46570dc1a
Logan Kearsley <chronosurfer@gmail.com>
2022-10-25 11:04:15 -0600

    Use size to reduce isNaN checks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+const stub = {
+  next(_: number) { return NaN; },
+  prev(_: number) { return NaN; },
+  *[Symbol.iterator]() {},
+} as unknown as VEB;
+
 export class VEB {
   private shift: number;
   private lo_mask: number;
   private cluster_size: number;
+  private cluster_count: number;
   private clusters: { [key: number]: VEB };
-  private summary: VEB;
+  private summary: VEB = stub;
   private min = NaN;
   private max = NaN;
+  private _size = 0;
   
   constructor(public readonly bound: number) {
     const shift = Math.floor(Math.log2(bound)/2);
@@ -18,64 +26,81 @@ export class VEB {
     this.cluster_size = cluster_size;
     this.clusters = {};
 
-    const cluster_count = Math.ceil(bound / cluster_size);
-    // can we add the summary structure lazily?
-    this.summary = bound > 2 ? new VEB(cluster_count) : null as any;
+    this.cluster_count = Math.ceil(bound / cluster_size);
     this.shift = shift;
   }
 
-  public insert(x: number) {
-    const { min, max } = this;
-    if (isNaN(min)) {
+  get size() { return this._size; }
+
+  public insert(x: number): boolean {
+    const { min, max, _size } = this;
+    if (x === min || x === max) { return false; }
+    if (_size === 0) {
       this.min = x;
       this.max = x;
-    } else if (x < min) {
+      this._size++;
+      return true;
+    }
+    
+    if (x < min) {
       this.min = x;
-      this.insert(min);
+      x = min;
     } else if (x > max) {
       this.max = x;
-      this.insert(max);
-    } else if (x !== min && x !== max) {
-      const i = x >>> this.shift;
-      if (!this.clusters.hasOwnProperty(i)) {
-        this.clusters[i] = new VEB(this.cluster_size);
-        this.summary.insert(i);
-      }
-      this.clusters[i].insert(x & this.lo_mask);
+      x = max;
     }
+
+    if (_size === 1) {
+      this._size++;
+      return true;
+    }
+
+    const { clusters } = this;
+    const i = x >>> this.shift;
+    if (!clusters.hasOwnProperty(i)) {
+      clusters[i] = new VEB(this.cluster_size);
+      if (this.summary === stub) {
+        this.summary = new VEB(this.cluster_count);
+      }
+      this.summary.insert(i);
+    }
+    const is_new = clusters[i].insert(x & this.lo_mask);
+    if (is_new) { this._size++; }
+    return is_new;
   }
 
-  public delete(x: number) {
+  public delete(x: number): boolean {
     let i: number;
     let j; Number;
     let cluster: VEB;
-    if (x === this.min) {
-      i = this.summary ? this.summary.min : NaN;
-      if (isNaN(i)) { // there are no sub-clusters
-        if (this.max === this.min) {
-          this.min = NaN;
+    const { min, max, _size } = this;
+    if (x === min) {
+      switch (_size) {
+        // if there are no sub-clusters
+        case 1:
           this.max = NaN;
-        } else {
+        case 2:
           this.min = this.max;
-        }
-        return;
+          this._size--;
+          return true;
       }
       // otherwise, pull the min up from one level down
+      i = this.summary.min;
       cluster = this.clusters[i];
       j = cluster.min;
       this.min = (i << this.shift) | j;
-    } else if (x === this.max) {
-      i = this.summary ? this.summary.max : NaN;
-      if (isNaN(i)) { // there are no sub-subclusters
-        if (this.max === this.min) {
+    } else if (x === max) {
+      switch (_size) {
+        // if there are no sub-clusters
+        case 1:
           this.min = NaN;
-          this.max = NaN;
-        } else {
+        case 2:
           this.max = this.min;
-        }
-        return;
+          this._size--;
+          return true;
       }
       // otherwise, pull the max up from one level down
+      i = this.summary.max;
       cluster = this.clusters[i];
       j = cluster.max;
       this.max = (i << this.shift) | j;
@@ -83,13 +108,18 @@ export class VEB {
       i = x >>> this.shift;
       j = x & this.lo_mask;
       cluster = this.clusters[i];
-      if (!cluster) { return; }
+      if (!cluster) { return false; }
     }
-    cluster.delete(j);
-    if (isNaN(cluster.min)) {
+    const had_x = cluster.delete(j);
+    if (cluster._size === 0) {
       this.summary.delete(i);
+      if (this.summary._size === 0) {
+        this.summary = stub;
+      }
       delete this.clusters[i];
     }
+    if (had_x) { this._size--; }
+    return had_x;
   }
 
   public next(x: number): number {
@@ -101,18 +131,16 @@ export class VEB {
       const j = x & this.lo_mask;
       if (j <= cluster.max) { return (i << shift) | cluster.next(j); }
     }
-    i = summary ? summary.next(i+1) : NaN;
+    i = summary.next(i+1);
     if (isNaN(i)) { return x <= max ? max : NaN; }
     return (i << shift) | clusters[i].min;
   }
 
   public has(x: number): boolean {
     if (x === this.min || x === this.max) { return true; }
-    let i = x >>> this.shift;
-    const { clusters } = this;
-    if (!clusters.hasOwnProperty(i)) { return false; }
-    const j = x & this.lo_mask;
-    return clusters[i].has(j);
+    const cluster = this.clusters[x >>> this.shift];
+    if (!cluster) { return false; }
+    return cluster.has(x & this.lo_mask);
   }
 
   public prev(x: number): number {
@@ -124,23 +152,21 @@ export class VEB {
       const j = x & this.lo_mask;
       if (j >= cluster.min) { return (i << shift) | cluster.prev(j); }
     }
-    i = summary && i > 0 ? summary.prev(i-1) : NaN;
+    i = i > 0 ? summary.prev(i-1) : NaN;
     if (isNaN(i)) { return x >= min ? min : NaN; }
     return (i << shift) | clusters[i].max;
   }
 
   public *keys(): Generator<number> {
-    const { min } = this;
-    if (isNaN(min)) { return; }
+    const { min, _size } = this;
+    if (_size === 0) { return; }
     yield min;
     const { max, clusters } = this;
     if (max > min) {
-      if (this.summary) {
-        const { shift } = this;
-        for (const i of this.summary) {
-          for (const j of clusters[i]) {
-            yield (i << shift) | j;
-          }
+      const { shift } = this;
+      for (const i of this.summary) {
+        for (const j of clusters[i]) {
+          yield (i << shift) | j;
         }
       }
       yield max;
--- test/exhaustive-test.ts
+++ test/exhaustive-test.ts
@@ -4,17 +4,22 @@ import { VEB } from '../src';
 
 describe("Random Tests", () => {
   const entries: number[] = [];
-  let x = 0;
-  for (let i = 0; i < 16; i++) {
-    x += 1 + Math.floor(4 * Math.random());
-    entries.push(x);
+  const outries: number[] = [];
+  {
+    let x = 0;
+    for (let i = 0; i < 16; i++) {
+      x += 1 + Math.floor(2 * Math.random());
+      entries.push(x);
+      x += 1 + Math.floor(2 * Math.random());
+      outries.push(x);
+    }
   }
 
   const v = new VEB(64);
 
   it("should add all the numbers to the tree", () => {
     for (const x of entries) {
-      v.insert(x);
+      expect(v.insert(x)).to.be.true;
       expect(v.has(x)).to.be.true;
     }
   });
@@ -23,6 +28,33 @@ describe("Random Tests", () => {
     for (const x of entries) {
       expect(v.has(x)).to.be.true;
     }
+  });
+
+  it("should not have elements that weren't added", () => {
+    for (const x of outries) {
+      expect(v.has(x)).to.be.false;
+    }
+  });
+
+  it("should have the right size", () => {
+    expect(v.size).to.eql(entries.length);
+  });
+
+  it("should be unchanged after inserting duplicates", () => {
+    for (const x of entries) {
+      expect(v.insert(x)).to.be.false;
+    }
+    expect(v.size).to.eql(entries.length);
+  });
+
+  it("should be unchanged after deleting missing items", () => {
+    for (const x of outries) {
+      expect(v.delete(x)).to.be.false;
+    }
+    for (const x of entries) {
+      expect(v.has(x)).to.be.true;
+    }
+    expect(v.size).to.eql(entries.length);
   });
   
   it("should iterate over the entries", () => {
@@ -43,9 +75,10 @@ describe("Random Tests", () => {
 
   it("should delete all the entries", () => {
     for (const x of entries) {
-      v.delete(x);
+      expect(v.delete(x)).to.be.true;
     }
     expect([...v]).to.eql([]);
+    expect(v.size).to.eql(0);
   });
 
 });
